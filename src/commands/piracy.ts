4f489c315f5e1252d10797a4e123a333cff8fa2b
Hung Hoang <hung.h.progdev@gmail.com>
2023-11-11 14:31:22 +0000

    added more acts

--- src/commands/piracy.ts
+++ src/commands/piracy.ts
@@ -12,7 +12,9 @@ const PiracyActs: string[] = [
   'conqeuror-haki',
   'true-nakama',
   'king',
-  'hoensty-impact'
+  'hoensty-impact',
+  'cat-burglar',
+  'shogun'
 ];
 
 const Bounties = new Map<string, number>([
@@ -25,125 +27,49 @@ const Bounties = new Map<string, number>([
   [PiracyActs[6], 5000],
   [PiracyActs[7], 3000],
   [PiracyActs[8], 3000],
-  [PiracyActs[9], 15000]
+  [PiracyActs[9], 15000],
+  [PiracyActs[10], 2000],
+  [PiracyActs[11], 3000]
 ]);
 
+const BountiesDescription: string[] = [
+  'Kill 7 Characters with Purple 10-drop Kaido',
+  'Kill any of: 10 drop Big Mom, 10 drop Kaido, 9 drop Shanks, 9 drop Edward Newgate',
+  'Play 4 searches',
+  'Playing a Black deck, K.O. 5 characters through effect',
+  'Jet Pistol a Pacifista',
+  'Have 5 characters that are different Straw Hats on the board',
+  'Have 5 different characters that canonically have Conquerors Haki',
+  'Block for your teammate in Buddy Battle',
+  'Win a tournament',
+  'K.O. a Stage',
+  'Win a game with OP03-040 Nami Leader secondary win con',
+  'Have 5 different characters that are different Wano characters'
+];
+
+const PiracyCommand = new SlashCommandBuilder()
+  .setName('piracy')
+  .setDescription('Report an act of piracy');
+
+for (let index = 0; index < PiracyActs.length; index++) {
+  const act = PiracyActs[index];
+  const description = BountiesDescription[index];
+
+  PiracyCommand.addSubcommand((subcommand) =>
+    subcommand
+      .setName(act)
+      .setDescription(description)
+      .addUserOption((option) =>
+        option
+          .setName('username')
+          .setDescription('Username of Pirate')
+          .setRequired(true)
+      )
+  );
+}
+
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('piracy')
-    .setDescription('Report an act of piracy')
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[0])
-        .setDescription('Kill 7 Characters with 10-drop Kaido')
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[1])
-        .setDescription('Kill 10-drop Big Mom')
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[2])
-        .setDescription('Play 4 searches')
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[3])
-        .setDescription('K.O. 5 characters through effect')
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[4])
-        .setDescription('Jet Pistol a Pacifista')
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[5])
-        .setDescription('Have 5 characters that is Straw Hat on the board')
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[6])
-        .setDescription(
-          'Have 5 characters that canonically have Conquerors Haki'
-        )
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[7])
-        .setDescription('Block for your teammate in Buddy Battle')
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[8])
-        .setDescription('Win a tournament')
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(PiracyActs[9])
-        .setDescription('K.O. a Stage')
-        .addUserOption((option) =>
-          option
-            .setName('username')
-            .setDescription('Username of Pirate')
-            .setRequired(true)
-        )
-    ),
+  data: PiracyCommand,
 
   async execute(interaction: ChatInputCommandInteraction) {
     await interaction.deferReply();
